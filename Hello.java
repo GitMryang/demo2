dce78f9d1f22717c9ca1aaa4f0799b36a567252b
mryang <yang@qq.com>
2021-12-24 15:48:22 +0800

    version 1.0

--- Hello.java
+++ Hello.java
@@ -1,7 +1,5 @@
 pulic class Hello{
 	pulic static void main(String[] args){
 		System.out.println("Hello world!");
-		System.out.println("GOOD NIGHT!");
-		System.out.println("GOOD MORNING!");
 	}
 }
